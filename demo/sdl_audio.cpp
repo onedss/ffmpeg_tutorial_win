14c91f10071624bfaf9f9f3b377eb18dfc6b08db
onedss <onedss@qq.com>
2022-04-07 15:40:53 +0800

    add first app sdl_audio.cpp.

---
+++ demo/sdl_audio.cpp
@@ -0,0 +1,12 @@
+//
+// Created by yzymi on 2022-04-05.
+//
+
+#include <iostream>
+#include <sdl/SDL.h>
+#undef main
+
+int main(int argc, char **argv) {
+    SDL_Init(SDL_INIT_AUDIO);
+    return 0;
+}
